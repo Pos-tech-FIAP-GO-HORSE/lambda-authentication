fbf59eb37be531eea36e75139fdbd9cbe613fb91
Thais Moura <thais.o.moura@gmail.com>
2025-01-02 16:56:55 -0300

    Reapplying changes from feat/add-authentication

---
+++ internal/handlers/handler.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/Pos-tech-FIAP-GO-HORSE/lambda-authorization/internal/core/usecases"
+	"github.com/aws/aws-lambda-go/events"
+	"net/http"
+)
+
+type AuthenticationHandler struct {
+	ValidationUserUseCase *usecases.AuthorizerUseCase
+}
+
+func NewAuthenticationHandler(validationUserUseCase *usecases.AuthorizerUseCase) *AuthenticationHandler {
+	return &AuthenticationHandler{
+		ValidationUserUseCase: validationUserUseCase,
+	}
+}
+
+func (h *AuthenticationHandler) Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	cpf := request.Headers["cpf"]
+	if cpf == "" {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       `{"message": "CPF header is missing"}`,
+		}, nil
+	}
+	authenticationResult, err := h.ValidationUserUseCase.AuthenticateUser(cpf)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       err.Error(),
+		}, err
+	}
+	return events.APIGatewayProxyResponse{
+		StatusCode:      http.StatusOK,
+		Body:            fmt.Sprintf(`{"message": "%s"}`, authenticationResult),
+		IsBase64Encoded: false,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}, nil
+}
---
+++ main.tf
@@ -0,0 +1,98 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_policy"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "cognito-idp:ListUsers",
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+        Effect   = "Allow"
+      },
+    ]
+  })
+}
+
+resource "aws_lambda_function" "auth_lambda" {
+  function_name = "auth_lambda"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "main"
+  runtime       = "provided.al2"
+
+  filename      = "function.zip"
+  source_code_hash = filebase64sha256("function.zip")
+}
+
+resource "aws_api_gateway_rest_api" "auth_api" {
+  name        = "auth-api"
+  description = "API Gateway para autenticação com Lambda"
+}
+
+resource "aws_api_gateway_resource" "auth_resource" {
+  rest_api_id = aws_api_gateway_rest_api.auth_api.id
+  parent_id   = aws_api_gateway_rest_api.auth_api.root_resource_id
+  path_part   = "auth"
+}
+
+resource "aws_api_gateway_method" "auth_method" {
+  rest_api_id   = aws_api_gateway_rest_api.auth_api.id
+  resource_id   = aws_api_gateway_resource.auth_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "auth_integration" {
+  rest_api_id = aws_api_gateway_rest_api.auth_api.id
+  resource_id = aws_api_gateway_resource.auth_resource.id
+  http_method = aws_api_gateway_method.auth_method.http_method
+  integration_http_method = "POST"
+  type       = "AWS_PROXY"
+  uri        = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.auth_lambda.arn}/invocations"
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  principal     = "apigateway.amazonaws.com"
+  function_name = aws_lambda_function.auth_lambda.function_name
+}
+
+resource "aws_cognito_user_pool" "authentication-cognito" {
+  name = "cpf-user-pool"
+}
+
+resource "aws_cognito_user_pool_client" "authentication_client" {
+  name         = "authentication-client"
+  user_pool_id = aws_cognito_user_pool.authentication-cognito.id
+  }
